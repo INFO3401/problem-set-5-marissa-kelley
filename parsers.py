ab2267892c0a37fd22c9cb66462039aeb06f4ed2
marissa-kelley <marissa.kelley@github.com>
2018-10-08 14:51:56 -0600

    Fixed Part 1, attempted Part 2 (for like the 10th time)

--- parsers.py
+++ parsers.py
@@ -4,25 +4,77 @@
 #I worked with Jacob Paul, Hannah Weber, Taylor Lawrence, Amy Zhai, Steven Rothaus 
 
 def countWordsUnstructured(filename):
-    wordcount = {}
-    file = open(filename)
-    for word in file.read().split():
+    wordcount = {} #initialize a word count dictionary
+    file = open(filename) #open the file
+    for word in file.read().split(): #split out into words and count the words
+        for mark in string.punctuation:
+            word = word.replace(mark, "")
         if word not in wordcount:
             wordcount[word] = 1
         else:
             wordcount[word] += 1
-    print (wordcount) 
+    print (wordcount) #print the word count dictionary 
          
     # This function should count the words in an unstructured text document
     # Inputs: A file name (string)
     # Outputs: A dictionary with the counts for each word
     # +1 bonus point for removing punctuation from the wordcounts
+import string
 
+#In class October 1st work
+#def countWordsUndstructured(filename):
+    #initalize a word count dictionary 
+    #wordCounts = {} 
+   
+    #open the file & read it 
+    #datafile = open(filename).read()
+    
+    #split out into words 
+    #data = datafile.split() 
+   
+    #Count the words
+    #for word in data: 
+        #for mark in string.punctuation:
+            #word = word.replace(mark, "")
+        #if word in wordCounts:
+            #wordCounts[word] = wordCounts[word] + 1
+        #else: 
+            #wordCounts[word] = 1
+    
+    #return the word count dictionary
+    #return wordCounts 
 ################################################################################
 # PART 2
 ################################################################################
+#I worked with Hannah Weber, Taylor Lawrence
+
+#import csv
+import csv
+
+def generateSimpleCSV(targetfile, wordCounts):  
+    wordCounts = []
+    #open the csv 
+    with open (targetfile, 'w') as csv_file:
+        #write to the csv using commas as a seperator 
+        writer = csv.writer(csv_file)
+    #make a header noting Word, Count
+    writer.writerow(['Word', 'Count'])
     
-#def generateSimpleCSV(targetfile, wordCounts): 
+    #make word count dictionary to a csv
+    for key, value in wordCounts.items():
+        writer.writerow([key,value[0]]) #SOMETHING IS FUNKY 
+   
+
+ #txt_file = r"state-of-the-union-corpus-1989-2017/Bush_1989.txt"
+    
+   #in_txt = csv.reader(open(txt_file, "rb"), delimerter = '\t')
+    #out_csv = csv.writer(open(csv_file, 'wb'))
+    
+    #out_csv.writewrows(in_txt)
+    #Create a header for the CSV 
+    
+generateSimpleCSV('PleaseWork', countWordsUnstructured('state-of-the-union-corpus-1989-2017/Bush_1989.txt'))
+
     # This function should transform a dictionary containing word counts to a
     # CSV file. The first row of the CSV should be a header noting: 
     # Word, Count
@@ -88,5 +140,5 @@ def countWordsUnstructured(filename):
 # +1 bonus point for figuring out how many datapoints you had to process to 
 # compute this value
 
-countWordsUnstructured("state-of-the-union-corpus-1989-2017/Bush_1989.txt")
+countWordsUnstructured('state-of-the-union-corpus-1989-2017/Bush_1989.txt')
 
